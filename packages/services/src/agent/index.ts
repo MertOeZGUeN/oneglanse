31c21d5e2f2c58e2d889ab84abd772028effc706
MertOeZGUeN <mevtbey@hotmail.com>
2026-09-14 13:57:32 +0300

    Export visibility prompt-set job runner

--- packages/services/src/agent/index.ts
+++ packages/services/src/agent/index.ts
@@ -1,5 +1,6 @@
 export * from "./queue.js";
 export * from "./redis.js";
 export * from "./jobs.js";
+export * from "./visibilityJobs.js";
 export * from "./auth.js";
 export * from "./progress.js";
